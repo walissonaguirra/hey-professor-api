04ff3b7fe16a6098093ce84c2507e5c89e22b043
Walisson Aguirra <walissonaguirra@proton.me>
2024-11-25 11:25:15 -0300

    test: it should allow that only creator can restore question

--- tests/Feature/Question/RestoreTest.php
+++ tests/Feature/Question/RestoreTest.php
@@ -22,5 +22,20 @@ it('should be able to retore a question', function () {
 
 });
 
-it('should be able to restore question')->todo();
-it('should allow that only creator can restore question')->todo();
+it('should allow that only creator can restore question', function () {
+
+    $user1    = User::factory()->create();
+    $user2    = User::factory()->create();
+    $question = Question::factory()->for($user1)->create();
+    $question->delete();
+
+    assertSoftDeleted('questions', ['id' => $question->id]);
+
+    Sanctum::actingAs($user2);
+
+    putJson(route('question.restore', $question))
+        ->assertForbidden();
+
+    assertSoftDeleted('questions', ['id' => $question->id]);
+
+});

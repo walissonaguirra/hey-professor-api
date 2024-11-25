1faca7d644ce3bdaa8568a2656fd13823242cc08
Walisson Aguirra <walissonaguirra@proton.me>
2024-11-25 11:12:15 -0300

    test: it should be able to retore a question

--- tests/Feature/Question/RestoreTest.php
+++ tests/Feature/Question/RestoreTest.php
@@ -1,5 +1,26 @@
 <?php
 
-it('should be able to retore a question')->todo();
+use App\Models\{Question, User};
+use Laravel\Sanctum\Sanctum;
+
+use function Pest\Laravel\{assertNotSoftDeleted, assertSoftDeleted, putJson};
+
+it('should be able to retore a question', function () {
+
+    $user     = User::factory()->create();
+    $question = Question::factory()->for($user)->create();
+    $question->delete();
+
+    assertSoftDeleted('questions', ['id' => $question->id]);
+
+    Sanctum::actingAs($user);
+
+    putJson(route('question.restore', $question))
+        ->assertNoContent();
+
+    assertNotSoftDeleted('questions', ['id' => $question->id]);
+
+});
+
 it('should be able to restore question')->todo();
 it('should allow that only creator can restore question')->todo();

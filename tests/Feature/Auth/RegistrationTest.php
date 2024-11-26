3f1c83a12e8a2474e0173406d9526576a71a8d7b
Walisson Aguirra <walissonaguirra@proton.me>
2024-11-26 08:51:26 -0300

    test: it should log the new user in the system

--- tests/Feature/Auth/RegistrationTest.php
+++ tests/Feature/Auth/RegistrationTest.php
@@ -3,7 +3,7 @@
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 
-use function Pest\Laravel\{assertDatabaseHas, postJson};
+use function Pest\Laravel\{assertAuthenticatedAs, assertDatabaseHas, postJson};
 use function PHPUnit\Framework\assertTrue;
 
 it('should be able to register in the application', function () {
@@ -22,5 +22,19 @@ it('should be able to register in the application', function () {
     $johnDoe = User::whereEmail('john@doe.com')->first();
 
     assertTrue(Hash::check('password', $johnDoe->password));
+
+});
+
+it('should log the new user in the system', function () {
+
+    postJson(route('register'), [
+        'name'     => 'John Doe',
+        'email'    => 'john@doe.com',
+        'password' => 'password',
+    ])->assertStatus(200);
+
+    $user = User::first();
+
+    assertAuthenticatedAs($user);
 
 });

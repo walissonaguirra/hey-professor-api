b62ccaf557b6b967dc7d0afc44edf5aa716f55ff
Walisson Aguirra <walissonaguirra@proton.me>
2024-11-26 14:01:38 -0300

    fix: move route unauthenticad to middleware guest

--- routes/api.php
+++ routes/api.php
@@ -5,18 +5,18 @@ use App\Http\Controllers\Auth\{LogoutController, RegisterController};
 use App\Http\Controllers\QuestionController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('register', RegisterController::class)->name('register');
-Route::post('login', LoginController::class)->name('login');
+Route::middleware('guest')->group(function () {
+    Route::post('register', RegisterController::class)->name('register');
+    Route::post('login', LoginController::class)->name('login');
+});
 
 Route::middleware(['web', 'auth'])->post('logout', LogoutController::class)->name('logout');
 
 Route::middleware('auth:sanctum')->group(function () {
-
     Route::post('questions', [QuestionController::class, 'store'])->name('question.store');
     Route::put('questions/{question}', [QuestionController::class, 'update'])->name('question.update');
     Route::delete('questions/{question}', [QuestionController::class, 'destroy'])->name('question.delete');
     Route::delete('questions/{question}/archive', [QuestionController::class, 'archive'])->name('question.archive');
     Route::put('questions/{question}/restore', [QuestionController::class, 'restore'])->name('question.restore');
     Route::put('questions/{question}/publish', [QuestionController::class, 'publish'])->name('question.publish');
-
 });
